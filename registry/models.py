37bf7949c7bdef609b85d654e8266b012678bc19
Shreyas Chand <shreyas.chand@gmail.com>
2012-04-13 12:03:09 -0400

    added user profile model

--- registry/models.py
+++ registry/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
+from django.contrib.localflavor.us import models as usmodels
 from django.db import models
 
-# Create your models here.
+class UserProfile (models.Model):
+    user = models.OneToOneField(User)
+    
+    specialty = models.CharField(max_length = 60)
+    url = models.URLField()
+    address = models.TextField()
+    phone_number = usmodels.PhoneNumberField()
