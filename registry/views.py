a9d0537cef31a145df91d0aa52d7f826b786bebf
Shreyas Chand <shreyas.chand@gmail.com>
2012-04-14 02:27:29 -0400

    Finished register view

--- registry/views.py
+++ registry/views.py
@@ -24,23 +24,24 @@ def server_detail(request, username):
         auth = authenticate(username=request.POST['username'], password=request.POST['password'])
         if auth is not None and auth.is_staff: 
             user = User.objects.get(username=username)
-            profile = user.get_profile()
-            response = 'Dr. ' + user.get_full_name() + '$';
-            response += profile.specialty + '$'
-            response += profile.url + '$'
-            response += profile.address + '$'
-            response += profile.phone_number + '$'
+            if not user.is_staff: 
+                profile = user.get_profile()
+                response = 'Dr. ' + user.get_full_name() + '$';
+                response += profile.specialty + '$'
+                response += profile.url + '$'
+                response += profile.address + '$'
+                response += profile.phone_number + '$'
         return HttpResponse(response)
     raise Http404
     
 @login_required
-def detail(request):
+def detail(request, new_user = False):
     if request.user.is_staff:
         return redirect('/MEDIC/admin')
     n = request.user.get_full_name()
     profile = request.user.get_profile()
     u = [profile.specialty, profile.address, profile.phone_number, ]
-    return render_to_response('detail.html', {'name' : n, 'user': u, 'url' : profile.url},
+    return render_to_response('detail.html', {'name' : n, 'user': u, 'url' : profile.url, 'new' : new_user},
                                context_instance=RequestContext(request))
     
 def register(request):
@@ -48,7 +49,14 @@ def register(request):
         userForm = UserRegistrationForm(request.POST)
         userProfileForm = UserProfileRegistrationForm(request.POST)
         if userForm.is_valid() and userProfileForm.is_valid():
-            return HttpResponse('Works')
+            user = User.objects.create_user(userForm.cleaned_data["username"], userForm.cleaned_data["email"], userForm.cleaned_data["password"])
+            user.first_name = userForm.cleaned_data["first_name"]
+            user.last_name = userForm.cleaned_data["last_name"]
+            user.save()
+            userProfile = userProfileForm.save(commit=False)
+            userProfile.user = user
+            userProfile.save()
+            return redirect('/MEDIC/success/')
     else:
         userForm = UserRegistrationForm()
         userProfileForm = UserProfileRegistrationForm()
